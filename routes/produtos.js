b4bef6fceae7182769c967b406aad4ca784cb3a8
Wesley  Jhonnes Ramos Rolim <wesley.rolim@ifro.edu.br>
2023-10-05 16:43:18 -0400

    jwt

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const multer = require('multer');
+const login = require('../middleware/login');
 
 const storage = multer.diskStorage({
     destination: function(req, file,cb) {
@@ -62,7 +63,8 @@ router.get('/',(req,res,next) => {
 });
 
 // Insere um produto
-router.post('/',upload.single('produto_imagem'),(req, res, next)=> {  
+router.post('/',login.obrigatorio,upload.single('produto_imagem'),(req, res, next)=> {  
+    console.log(req.usuario); 
     console.log(req.file);  
     mysql.getConnection((error,conn) => {
         if(error) {return res.status(500).send({error:error})}
@@ -144,7 +146,7 @@ router.get('/:id_produto',(req,res,next) => {
 
 
 // Atualiza um produto
-router.patch('/',(req, res, next)=> {
+router.patch('/',login.obrigatorio,(req, res, next)=> {
     mysql.getConnection((error,conn) => {
         if(error) {return res.status(500).send({error:error})}
         conn.query(
@@ -185,7 +187,7 @@ router.patch('/',(req, res, next)=> {
 });
 
 // Deleta um produto
-router.delete('/',(req, res, next)=> {
+router.delete('/',login.obrigatorio,(req, res, next)=> {
     mysql.getConnection((error,conn) => {
         if(error) {return res.status(500).send({error:error})}
         conn.query(
